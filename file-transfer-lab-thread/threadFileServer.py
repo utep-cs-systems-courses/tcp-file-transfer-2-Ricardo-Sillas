7d8f702ed3c636feab4b7253f0c4c6179d75d873
Ricardo-Sillas <ricachan22000@gmail.com>
2020-10-18 19:21:07 -0600

    threads

---
+++ file-transfer-lab-thread/threadFileServer.py
@@ -0,0 +1,68 @@
+#! /usr/bin/env python3
+
+import sys
+sys.path.append("../lib")       # for params
+import re, socket, params, os
+sys.path.append("../framed-echo")
+from framedSock import framedSend, framedReceive
+
+switchesVarDefaults = (
+    (('-l', '--listenPort') ,'listenPort', 50001),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+
+progname = "echoserver"
+paramMap = params.parseParams(switchesVarDefaults)
+
+debug, listenPort = paramMap['debug'], paramMap['listenPort']
+
+if paramMap['usage']:
+    params.usage()
+
+lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # listener socket
+bindAddr = ("127.0.0.1", listenPort)
+lsock.bind(bindAddr)
+lsock.listen(5)
+
+
+class Server(Thread):
+    def __init__(self, sockAddr):
+        Thread.__init__(self)
+        self.sock, self.addr = sockAddr
+    def run(self):
+        print("new thread handling connection from", self.addr)
+        while True:
+            print("listening on:", bindAddr)
+
+            sock, addr = lsock.accept()
+    
+            print("connection rec'd from", addr)
+
+            payload = framedReceive(sock, debug)
+            if debug:
+                print("rec'd: ", payload)
+
+            if not payload:
+                sys.exit(1)
+            
+            payload = payload.decode()
+
+            name = payload.split("<")[0]
+            content = payload.split("<")[1].encode()
+            
+            try:
+                if not os.path.isfile(name):
+                    file = open(name, 'wb+')
+                    file.write(content)
+                    file.close()
+                else:
+                    print("File with name", name, "already exists on server. exiting...")
+            except FileNotFoundError:
+                print("Fail")
+
+
+while True:
+    sockAddr = lsock.accept()
+    server = Server(sockAddr)
+    server.start()
---
+++ file-transfer-lab-thread/transfer/fileClient.py
@@ -0,0 +1,57 @@
+#! /usr/bin/env python3
+
+import socket, sys, re
+
+sys.path.append("../../lib")       # for params
+import params
+
+sys.path.append("../../framed-echo")
+from framedSock import framedSend, framedReceive
+
+switchesVarDefaults = (
+    (('-s', '--server'), 'server', "127.0.0.1:50001"),
+    (('-d', '--debug'), "debug", False), # boolean (set if present)
+    (('-?', '--usage'), "usage", False), # boolean (set if present)
+    )
+    
+progName = "fileClient"
+paramMap = params.parseParams(switchesVarDefaults)
+
+server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+
+if usage:
+    params.usage()
+    
+try:
+    serverHost, serverPort = re.split(":", server)
+    serverPort = int(serverPort)
+except:
+    print("Can't parse server:port from '%s'" % server)
+    sys.exit(1)
+
+addrFamily = socket.AF_INET
+socktype = socket.SOCK_STREAM
+addrPort = (serverHost, serverPort)
+
+s = socket.socket(addrFamily, socktype)
+
+if s is None:
+    print('could not open socket')
+    sys.exit(1)
+
+s.connect(addrPort)
+try:
+    userInput = input("Please put a text file you would like to send.\n")
+    fc = open(userInput, "rb")
+except FileNotFoundError:
+    print("  not found")
+    sys.exit(1)
+
+words = fc.read()
+
+if len(words) == 0:
+    print("Contents of file were empty")
+    sys.exit(1)
+
+print("sending " + userInput)
+framedSend(s, (userInput + "<" + str(words)).encode(), debug)
